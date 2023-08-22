925a3472cdc577db02e9756b94b76385a2c01fd9
Serpent03 <dumbpilot03@gmail.com>
2023-08-22 14:51:07 +0530

    init

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+// #include <graphics.h>
+// #include <SDL2/SDL.h>
+
+// SDL2 is to be used. let's get it started first,
+// then we will start making the orbital simulator or even a matplotlib clone !
+
+int main()
+{
+    
+}
+
+
