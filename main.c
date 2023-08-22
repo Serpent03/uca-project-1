8bcba6ad11ad93d8fbb27fd357c69b622f95a7e2
Serpent03 <dumbpilot03@gmail.com>
2023-08-22 23:03:21 +0530

    setup

--- main.c
+++ main.c
@@ -6,9 +6,31 @@
 // SDL2 is to be used. let's get it started first,
 // then we will start making the orbital simulator or even a matplotlib clone !
 
+#define WIDTH 80
+#define HEIGHT 40
+#define T_PIXEL WIDTH * HEIGHT
+int BITMAP[T_PIXEL];
+
+void RESET()
+{
+    for (int i = 0; i < T_PIXEL; i++)
+    {
+        BITMAP[i] = 0;
+    }
+}
+
+void printA(int* pArr, int pArrLen)
+{
+    for (int i = 0; i < pArrLen; i++)
+    {
+        printf("->%d\n", pArr[i]);
+    }
+}
+
 int main()
 {
-    
+    printf("%d", T_PIXEL);
+    printA(BITMAP, T_PIXEL);
 }
 
 
