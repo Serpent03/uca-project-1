e613d1bb1ad26ddd0c4a8e7cd81935e937d781b3
Serpent03 <dumbpilot03@gmail.com>
2023-08-23 16:01:17 +0530

    feat: rasterization of PNGs into bitmaps

---
+++ rasterization.py
@@ -0,0 +1,34 @@
+import cv2
+from os import listdir
+
+# read imgs, resize to
+# definition sizes
+
+rw, rh = 60, 25
+
+imgPath = './img/'
+outPath = './out/'
+
+inPath = listdir(imgPath)
+ctr = 1
+
+for file in inPath:
+    print(f'{imgPath}{file}')
+    im = cv2.imread(f'{imgPath}{file}')
+    grayImage = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+  
+    (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
+    goodRes = cv2.resize(blackAndWhiteImage, (rw, rh), interpolation=cv2.INTER_LINEAR)
+
+    with open(f'{outPath}{ctr}.txt', 'w+') as f:
+        for height in goodRes:
+            for width in height:
+                num = 1 if width == 255 else 0
+                f.write(f"{num},")
+    ctr += 1
+
+# print(len(goodRes))
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
